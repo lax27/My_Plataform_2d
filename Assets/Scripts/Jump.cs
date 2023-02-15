a974f38d2f0a3031587314977941975954bee013
PC-clase-A <alex.taravilla@enti.cat>
2023-02-15 12:01:32 +0100

    PULIDO DEL SALTO + MENU DE NIVELES

--- Assets/Scripts/Jump.cs
+++ Assets/Scripts/Jump.cs
@@ -4,21 +4,48 @@ using UnityEngine;
 
 public class Jump : MonoBehaviour
 {
+    public int TopJump = 1;
     Rigidbody2D rb;
+    GroundDetector gr;
     public float force;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        gr = GetComponent<GroundDetector>();
     }
 
     // Update is called once per frame
     void Update()
     {
         if (!PauseMenu.instance.isPaused)
-            if (Input.GetButtonDown("Jump"))
+
+            if(gr.grounded == true)
             {
-                rb.AddForce(Vector2.up * force);
+                TopJump = 1;
             }
+            
+       if (Input.GetButtonDown("Jump"))
+       {
+            //primer salto
+        if(gr.grounded == true && TopJump != 0)
+        {
+                    rb.AddForce(Vector2.up * force);
+        }
+        //segundo salto
+        else if (gr.grounded != true && TopJump != 0)
+        {
+                TopJump--;
+                    rb.AddForce(Vector2.up * force * 1.5f);
+        }
+       else
+       {
+
+       }
+        
+       }
+          
+            
+            
     }
 }
